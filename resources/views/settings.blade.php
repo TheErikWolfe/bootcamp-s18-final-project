9f25539d7407e738ac960eeac6dd02e47b633678
Erik Wolfe <theerikwolfe@gmail.com>
2018-07-02 14:50:27 -0400

    Added sections for settings

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -3,7 +3,26 @@
 @section('content')
 
 <div class="container">
-    <h1>Hello from settings page</h1>
+    <div class="row">
+        <div class="card col-4 m-2 float-left">
+            <ul class="nav flex-column">
+                <li class="nav-item m-auto">
+                    <a class="nav-link active" href="#">Profile</a>
+                </li>
+                <li class="nav-item m-auto">
+                    <a class="nav-link active" href="#">Account</a>
+                </li>
+                <li class="nav-item m-auto">
+                    <a class="nav-link active" href="#">Preferences</a>
+                </li>
+            </ul>
+        </div>
+        <div class="card m-2 col-7">
+            <div class="card-body">
+                @yield('settings-content')
+            </div>
+        </div>
+    </div>
 </div>
 
 @endsection
