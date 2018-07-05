20bdc3244923eaad3a6795e9690cf8c9f018039d
Erik Wolfe <theerikwolfe@gmail.com>
2018-07-05 19:37:28 -0400

    Added the sidebar and Made a for loop to repeat the home page images

--- resources/views/pages/settings.blade.php
+++ resources/views/pages/settings.blade.php
@@ -4,20 +4,22 @@
 
 <div class="container">
     <div class="row">
-        <div class="settings-menu-card card col col-md-4 col-sm-12 m-2 float-left">
-            <ul class="nav m-auto text-center flex-column">
-                <li class="nav-item">
-                    <a class="nav-link active" href="{{ route('profile') }}">{{ __('Profile') }}</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="{{ route('account') }}">{{ __('Account') }}</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="{{ route('preferences') }}">{{ __('Preferences') }}</a>
-                </li>
-            </ul>
-        </div>
-        <div class="settings-card card m-2 col col-md-7 col-sm-12">
+        <nav class="col-md-3 col-sm-12 p-2 h-75 mr-5 m-2 settings-menu-navbar d-none d-md-block sidebar navbar-laravel">
+            <div class="sidebar-sticky">
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="{{ route('profile') }}">{{ __('Profile') }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="{{ route('account') }}">{{ __('Account') }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="{{ route('preferences') }}">{{ __('Preferences') }}</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="settings-card card h-75 p-2 m-2 col-md-7 col-sm-12">
             <div class="card-body">
                 @yield('settings-content')
             </div>
