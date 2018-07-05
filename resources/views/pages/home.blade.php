036f2e38449a40b52bda0ff9238e8019fb9993da
Erik Wolfe <theerikwolfe@gmail.com>
2018-07-05 15:12:05 -0400

    Added a modal to one of the pictures to show how I want it to be.

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,12 +3,21 @@
 @section('content')
 
 <div class="container">
+    <!-- The Modal -->
+    <div id="myModal" class="modal">
+        <!-- The Close Button -->
+        <span class="close">&times;</span>
+
+        <!-- Modal Content (The Image) -->
+        <img class="modal-content" id="img01">
+    </div>
+
     <div class="row">
 
 
         <div class="imgframe">
             <div class="p-0">
-                <img class="imgprops" src="{{ asset('img/4.jpg') }}" />
+                <img id="myImg" class="imgprops" src="{{ asset('img/4.jpg') }}" style="width:100%;max-width:300px"/>
                 <div class="arrowcontainer">
                     <div class="arrow fa fa-arrow-up upvotearrow"></div>
                     <div class="arrow fa fa-arrow-down downvotearrow"></div>
