0ef14bb7432b17108853deceb63f8b4febaaa261
Erik Wolfe <theerikwolfe@gmail.com>
2018-07-06 12:05:13 -0400

    Made a very basic carousel inside of a modal.

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,13 +3,29 @@
 @section('content')
 
 <div class="">
-    <!-- The Modal -->
-    <div id="myModal" class="modal">
-        <!-- The Close Button -->
-        <span class="close">&times;</span>
-
-        <!-- Modal Content (The Image) -->
-        <img class="modal-content" id="img01">
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+        <div class="modal-content bg-transparent border-0">
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="p-0">
+                            <img id="" class="d-block w-75 m-auto" src="{{ asset('img/4.jpg') }}" alt="First slide"/>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+</div>
+        </div>
     </div>
 
     @for ($i = 0; $i < 10; $i++)
@@ -17,10 +33,10 @@
         @for ($j = 0; $j < 6; $j++)
         <div class="imgframe">
             <div class="p-0">
-                <img id="myImg" class="imgprops" src="{{ asset('img/4.jpg') }}"/>
+            <img type="button" data-toggle="modal" data-target="#exampleModal" id="myImg" class="imgprops" src="{{ asset('img/4.jpg') }}"/>
                 <div class="arrowcontainer">
-                    <div class="arrow fa fa-arrow-up upvotearrow"></div>
-                    <div class="arrow fa fa-arrow-down downvotearrow"></div>
+                    <div class="arrow bg-transparent fa fa-arrow-up upvotearrow"></div>
+                    <div class="arrow bg-transparent fa fa-arrow-down downvotearrow"></div>
                 </div>
             </div>
         </div>
