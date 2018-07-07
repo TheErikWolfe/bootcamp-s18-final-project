39566d4fe18173bb9e911ecac06f569428036c92
Erik Wolfe <theerikwolfe@gmail.com>
2018-07-07 17:20:27 -0400

    Starting to stylize around the canvas.

--- resources/views/pages/drawingapp.blade.php
+++ resources/views/pages/drawingapp.blade.php
@@ -3,31 +3,17 @@
 @section('content')
 
 <!-- <drawing-board></drawing-board> -->
-<div style="
-    background: #263960;
-    position: relative;
-    width: 554px;
-    height: 601px;
-    margin-right: 0%;
-    top: 10%;
-    right: 0;
-    left: 0;
-    margin-right: auto;
-    margin-left: auto;">
-        <div style="display:inline-block;
-        width: 150px;
-        height:600px;
-        vertical-align: top;
-        ">
-        <div class="wrapper">
-            <div class="canvas-button"><img src="https://png.icons8.com/ios/40/000000/paint-brush.png"></div>
-            <div class="canvas-button"><img src="https://png.icons8.com/ios/40/000000/fill-color.png"></div>
-            <div class="canvas-button"><img src="https://png.icons8.com/ios/40/000000/erase-filled.png"></div>
-            <div class="canvas-button"><img src="https://png.icons8.com/ios/40/000000/pencil.png"></div>
-        </div>
+<div class="drawing-app-wrapper">
+        <div class="drawing-app-button-navbar">
+            <div class="button-wrapper">
+                <div class="canvas-button"><img class="button-image" height="35px" width="35px" src="https://png.icons8.com/ios/40/000000/paint-brush.png"></div>
+                <div class="canvas-button"><img class="button-image" height="35px" width="35px" src="https://png.icons8.com/ios/40/000000/fill-color.png"></div>
+                <div class="canvas-button"><img class="button-image" height="35px" width="35px" src="https://png.icons8.com/ios/40/000000/erase-filled.png"></div>
+                <div class="canvas-button"><img class="button-image" height="35px" width="35px" src="https://png.icons8.com/ios/40/000000/pencil.png"></div>
+            </div>
         </div>
         <div style="display:inline-block;">
-            <canvas id="canvas"
+            <canvas id="drawing-app-canvas"
                 v-on:mousedown="handleMouseDown" 
                 v-on:mouseup="handleMouseUp" 
                 v-on:mousemove="handleMouseMove" 
